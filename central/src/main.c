09334bd192f402adb43357d71675d027d0c7be54
Pedro H <phbn@ic.ufal.br>
2023-04-16 21:53:12 -0300

    add: read from console

--- central/src/main.c
+++ central/src/main.c
@@ -1,5 +1,30 @@
+#include <console/console.h>
+#include <string.h>
+#include <sys/printk.h>
+#include <version.h>
 #include <zephyr.h>
 
-void main(void)
-{
+void main(void) {
+  printk("Hello! I'm using Zephyr %s on %s, a %s board. \n\n",
+         KERNEL_VERSION_STRING, CONFIG_BOARD, CONFIG_ARCH);
+
+  console_getline_init();
+  printk("Enter al line finishing with Enter:\n");
+
+  while (1) {
+    printk(">");
+    char *s = console_getline();
+
+    printk("Typed line: %s\n", s);
+    printk("Last char was: 0x%x\n", s[strlen(s) - 1]);
+  }
 }
+
+/**
+ * @brief Renode CLI
+ *
+ */
+// renode -e "include @scripts/single-node/nrf52840.resc;emulation
+// CreateServerSocketTerminal 1234 'externalUART';connector Connect sysbus.uart0
+// externalUART;sysbus LoadELF
+// @C:\Users\Pedro\Repos\projeto-embarcados-ble\central\.pio\build\nrf52840_dk\firmware.elf;start"
